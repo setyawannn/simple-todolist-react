b96076ecff57db19e344c4b0dba9bf4c0eab93a2
setyawannn <prayogasetyawan.adi@gmail.com>
2022-11-26 10:50:09 +0700

    Add: cancel edit, warning form, and  done  todo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   const [activity, setActivity] = React.useState('');
   const [edit, setEdit] = React.useState({});
   const [todos, setTodos] = React.useState([]);
+  const [message, setMessage] = React.useState('');
 
   function generateId() {
     return Date.now();
@@ -13,9 +14,15 @@ function App() {
   function saveTodoHandler(e) {
     e.preventDefault();
 
+    if (!activity) {
+      return setMessage('Nama aktivitas jangan kosong');
+    }
+
+    setMessage('');
+
     if (edit.id) {
-      const updateTodo = {
-        id: edit.id,
+      const updatedTodo = {
+        ...edit,
         activity
       }
       const editTodoIndex = todos.findIndex(function (todo) {
@@ -23,15 +30,16 @@ function App() {
       });
 
       const updatedTodos = [...todos];
-      updatedTodos[editTodoIndex] = updateTodo;
+      updatedTodos[editTodoIndex] = updatedTodo;
 
       setTodos(updatedTodos);
-      return;
+      return cancelEditHandler();
     }
 
     setTodos([...todos, {
       id: generateId(),
       activity,
+      done: false,
     }]);
     setActivity('');
   }
@@ -41,6 +49,8 @@ function App() {
       return todo.id !== todoId;
     });
     setTodos(filteredTodos);
+
+    if (edit.id) cancelEditHandler();
   }
 
   function editTodoHandler(todo) {
@@ -48,10 +58,33 @@ function App() {
     setEdit(todo);
   }
 
+  function cancelEditHandler() {
+    setEdit({});
+    setActivity('');
+  }
+
+  function doneTodoHandler(todo) {
+    const updatedTodo = {
+      ...todo,
+      done: todo.done ? false : true,
+    };
+
+    const editTodoIndex = todos.findIndex(function (currentTodo) {
+      return currentTodo.id == todo.id;
+    });
+
+    const updatedTodos = [...todos];
+    updatedTodos[editTodoIndex] = updatedTodo;
+
+    setTodos(updatedTodos);
+    // console.log(updatedTodos);
+  }
+
   return (
     <div className='bg'>
       <div className='container'>
         <h1>Simple todolist</h1>
+        {message && <div style={{ color: 'red' }}>{message}</div>}
         <form onSubmit={saveTodoHandler}>
           <input
             type='text'
@@ -63,20 +96,31 @@ function App() {
           <button type='submit'>
             {edit.id ? 'Simpan perubahan' : 'Tambah'}
           </button>
+          {edit.id && <button
+            onClick={cancelEditHandler}>Batal Edit</button>}
         </form>
-        <ul>
-          {todos.map(function (todo) {
-            return (
-              <li key={todo.id}>
-                {todo.activity}
-                <button onClick={editTodoHandler.bind(this, todo)}>Edit</button>
-                <button onClick={removeTodoHandler.bind(this, todo.id)}>Hapus</button>
-              </li>
-            )
-          })}
-        </ul>
+        {todos.length > 0 ? (
+          <ul>
+            {todos.map(function (todo) {
+              return (
+                <li key={todo.id}>
+                  <input
+                    type='checkbox'
+                    checked={todo.done}
+                    onChange={doneTodoHandler.bind(this, todo)}
+                  />
+                  {todo.activity}({todo.done ? 'Selesai' : 'Belum selesai'})
+                  <button onClick={editTodoHandler.bind(this, todo)}>Edit</button>
+                  <button onClick={removeTodoHandler.bind(this, todo.id)}>Hapus</button>
+                </li>
+              )
+            })}
+          </ul>
+        ) : (
+          <p><i>Tidak ada todo</i></p>
+        )}
       </div>
-    </div>
+    </div >
   );
 }
 
